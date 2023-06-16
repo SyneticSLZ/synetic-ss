ce979f97c258f5009b7d203cfd45a55ccb27be29
Synetic1 <sinistersouls2922@gmail.com>
2023-06-16 12:13:34 +0100

    navbar remove scrollbar

--- src/components/Hero/HeroHome.jsx
+++ src/components/Hero/HeroHome.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from "react";
-import "../Hero/Hero.css"
+import "../Hero/Home.css"
 import {  animated, useSpring } from 'react-spring';
 
 
@@ -14,33 +14,15 @@ const style1 = useSpring({
 
 });
 
-// const style2 = useSpring({
-//     from: { marginLeft: -400 },
-//      to:  {  marginLeft: 400 },
-//       config: { duration:1500 }
-
-// });
-
 
 
     return(
         
         <animated.div id="hero" className="hero hbody" style={ style1 }>
                <script src="https://app.vagon.io/vagonsdk.js"></script> 
-                {/* <iframe src="https://cdn.htmlgames.com/President/" frameborder="0" loading="lazy" allow="fullscreen"></iframe>  */}
-                {/* <div style={{ width: '100%', overflow: 'hidden' }}> */}
-  {/* <div style={{ position: 'relative', padding: '56.25% 0 0 0', overflow: 'hidden' }}> */}
-    {/* <iframe
-      src="https://cdn.htmlgames.com/President/"
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope;"
-      style={{ position: 'absolute', top: '110px', left: '0', width: '100%', height: '100%' }}
-    ></iframe> */}
 
-<iframe class="hvideo" id="vagonFrame" allow="microphone  *; clipboard-read *; clipboard-write *; encrypted-media *;accelerometer; autoplay; gyroscope;" src="https://streams.vagon.io/streams/805c2d1b-87b4-4c36-9922-454ad49f74ff"
-      style={{ position: 'absolute', top: '100px', left: '0', width: '100%', height: '100%' }}/>
-  {/* </div> */}
-{/* </div>     */}
+<iframe class="hvideo" id="vagonFrame" allow="microphone  *; clipboard-read *; clipboard-write *; encrypted-media *;accelerometer; autoplay; gyroscope; controls;" src="https://streams.vagon.io/streams/805c2d1b-87b4-4c36-9922-454ad49f74ff"
+      style={{ position: 'absolute', top: '100px', left: '0', width: '100%', height: '100vh' }}/>
                 </animated.div>
     )
 }
